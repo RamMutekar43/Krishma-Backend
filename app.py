edb7252e00e5da9d4545aa1088ef2411100d66d3
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-12-19 00:26:16 +0530

    commit

--- app.py
+++ app.py
@@ -812,4 +812,5 @@ def forecast_sales():
 
 # ---------- Fix: Correct entry point ----------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True,port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
